4fc244f8253d9e45720330ffc8ce06e137462a22
unknown <foruozanfar2000f@gmail.com>
2022-09-14 12:19:33 +0430

    init first

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    function Add()
+    {
+
+    }
+
+    function Remove()
+    {
+
+    }
+
+    function Edit()
+    {
+
+    }
+
+    function GetAll()
+    {
+
+    }
+
+    function GetByCategory()
+    {
+
+    }
+
+    function GetByFilter()
+    {
+
+    }
+}
+
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    function SignIn(Request $request)
+    {
+        
+    }
+
+    function SignOut()
+    {
+
+    }
+
+    function SignUp()
+    {
+
+    }
+
+    function EditProfile()
+    {
+
+    }
+
+    function ResetPassword()
+    {
+
+    }
+
+    function ShowComments()
+    {
+
+    }
+}
